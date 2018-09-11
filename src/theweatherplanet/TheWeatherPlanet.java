744bcf24dd0bb89a39fd330ca27be82130980307
Philip Garrett <pgarrett@AMCOs-MacBook-Pro.local>
2018-09-11 00:41:51 -0300

    Update in git ignore. Fix in arguments checking

--- src/theweatherplanet/TheWeatherPlanet.java
+++ src/theweatherplanet/TheWeatherPlanet.java
@@ -57,7 +57,7 @@ public class TheWeatherPlanet {
     }
 
     public static void main(final String[] args) {
-	if (args.length > 0 && args[0].equals(Constants.V_ARG) || args[0].equals(Constants.VERSION_ARG)) {
+	if (args.length > 0 && (args[0].equals(Constants.V_ARG) || args[0].equals(Constants.VERSION_ARG))) {
 	    System.out.println(Constants.INIT_MESSAGE);
 	    System.exit(0);
 	}
